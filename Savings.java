6654ade37fe76c01191e5d958b0a3340dfa593cb
CullMay <34067482+CullMay@users.noreply.github.com>
2018-11-11 13:20:16 -0600

    Add files via upload
    Just finished with classes

---
+++ Savings.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package accountinheritance;
+
+/**
+ *
+ * @author Fabio
+ */
+public class Savings extends Account {
+    static final double rate = .04;
+    
+    public Savings(double init){
+        
+        super (init);
+    }
+    
+    public double CalculateInterest(){
+        
+        double bal = getBalance();
+        double interest = bal*rate;
+        return interest;
+    }
+}
